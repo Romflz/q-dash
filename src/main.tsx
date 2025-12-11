512952161eebe9718d57cb2fbf8a9401ee3fff52
romflz <electronicmusicplease@gmail.com>
2025-12-12 00:53:42 +0100

    feat: import aliases

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import { RouterProvider } from 'react-router'
 import { router } from './router'
 import './index.css'
 
-console.log('main.tsx loaded')
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router'
-import HomePage from './pages/HomePage'
-import MainLayout from './layouts/MainLayout'
+import HomePage from '@/pages/HomePage'
+import MainLayout from '@/layouts/MainLayout'
 
 export const router = createBrowserRouter([
   {
